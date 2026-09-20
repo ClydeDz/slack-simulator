581c3fc5962299b939c67a32c3ab5dd972c05a8c
Clyde D'Souza <clyd94@gmail.com>
2026-09-20 17:37:28 +1200

    Add licensing comment to the main file

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import { useStore } from "./store";
-import "./themes/slack-light.css";
-import "./themes/slack-dark.css";
+// Copyright (C) 2026 CLYDE D'SOUZA
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import { useStore } from './store';
+import './themes/slack-light.css';
+import './themes/slack-dark.css';
 
 const queryClient = new QueryClient();
 
